5abb93b8582d43e8ad787708b6b4135d11a2fc01
Dillon Huff <dillonhuff@gmail.com>
2014-03-02 12:10:22 -0800

    applicative form conversion passing tests

--- src/ApplicativeForm.hs
+++ src/ApplicativeForm.hs
@@ -1,2 +1,18 @@
-module ApplicativeForm(toApForm) where
+module ApplicativeForm(
+	AExpr(AAp, ANum, AFun),
+	toApForm) where
 
+import Parsing
+
+data AExpr
+	= AAp AExpr AExpr
+	| ANum Int
+	| AFun String
+	deriving (Eq, Show)
+
+toApForm :: Expr -> AExpr
+toApForm (Num val) = ANum val
+toApForm (FunCall name []) = AFun name
+toApForm (FunCall name args) = foldl aapTree (AFun name) (map toApForm args)
+	where
+		aapTree apTree nextTree = AAp apTree nextTree
--- test/ApplicativeFormTests.hs
+++ test/ApplicativeFormTests.hs
@@ -1,11 +1,37 @@
 module ApplicativeFormTests() where
 
+import ApplicativeForm
 import Parsing
 import Test.HUnit
 
 applicativeFormTests = runTestTT tests
 
-tests = TestList []
+tests = TestList [
+	toApForm_num,
+	toApForm_num2,
+	toApForm_oneArgFunc,
+	toApForm_oneNestedArgFunc,
+	toApForm_twoArgFunc]
 
+numExpr = Num 980
 toApForm_num = TestCase
-	(assertEqual "number" (Num 980) (toApForm
+	(assertEqual "number 980" (ANum 980) (toApForm numExpr))
+	
+numExpr2 = Num 111
+toApForm_num2 = TestCase
+	(assertEqual "number 111" (ANum 111) (toApForm numExpr2))
+	
+toApForm_oneArgFunc = TestCase
+	(assertEqual "one argument function"
+		(AAp (AFun "f") (ANum 12))
+		(toApForm (FunCall "f" [(Num 12)])))
+		
+toApForm_oneNestedArgFunc = TestCase
+	(assertEqual "one nested argument"
+		(AAp (AFun "f") (AAp (AFun "g") (ANum 3)))
+		(toApForm (FunCall "f" [(FunCall "g" [(Num 3)])])))
+		
+toApForm_twoArgFunc = TestCase
+	(assertEqual "two args"
+		(AAp (AAp (AFun "hello") (ANum 4)) (ANum 7))
+		(toApForm (FunCall "hello" [(Num 4), (Num 7)])))
